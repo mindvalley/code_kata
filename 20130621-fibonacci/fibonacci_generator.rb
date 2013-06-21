d750b2364406a3335f2ffe44ded6229e8a049eaa
Tristan <tristan.gomez@gmail.com>
2013-06-21 16:49:18 +0800

    refactored out the duplications

--- 20130621-fibonacci/fibonacci_generator.rb
+++ 20130621-fibonacci/fibonacci_generator.rb
@@ -9,36 +9,9 @@ class FibonacciGenerator
     result = 0
     threshold = n - 2
 
-    if n==2
-      while n > threshold
-        n = n - 1
-        result += generate(n)
-      end
-    elsif n==3
-      while n > threshold
-        n = n - 1
-        result += generate(n)
-      end
-    elsif n==4
-      while n > threshold
-        n = n - 1
-        result += generate(n)
-      end
-    elsif n==5
-      while n > threshold
-        n = n - 1
-        result += generate(n)
-      end
-    elsif n==6
-      while n > threshold
-        n = n - 1
-        result += generate(n)
-      end
-    elsif n==7
-      while n > threshold
-        n = n - 1
-        result += generate(n)
-      end
+    while n > threshold
+      n = n - 1
+      result += generate(n)
     end
     result
   end
