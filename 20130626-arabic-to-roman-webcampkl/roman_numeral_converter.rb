da52c0be2535cc26f28913767f87d8411b80c6e3
Tristan <tristan.gomez@gmail.com>
2013-06-26 16:15:52 +0800

    make repetition more obvious

--- 20130626-arabic-to-roman-webcampkl/roman_numeral_converter.rb
+++ 20130626-arabic-to-roman-webcampkl/roman_numeral_converter.rb
@@ -6,19 +6,19 @@ class RomanNumeralConverter
       number -= 10
     end
 
-    if number == 9
+    while number >= 9
       result << "IX"
       number -= 9
     end
 
-    if number == 4
-      result << "IV"
-      number -= 4
-    end
-
-    if number >= 5
+    while number >= 5
       result << "V"
       number -= 5
+    end
+
+    while number >= 4
+      result << "IV"
+      number -= 4
     end
 
     while number >= 1
