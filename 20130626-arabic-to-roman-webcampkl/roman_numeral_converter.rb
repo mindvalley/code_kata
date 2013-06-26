21d7ee4739b6813d62a637871fcd819cc3393e84
Tristan <tristan.gomez@gmail.com>
2013-06-26 15:51:04 +0800

    make test pass

--- 20130626-arabic-to-roman-webcampkl/roman_numeral_converter.rb
+++ 20130626-arabic-to-roman-webcampkl/roman_numeral_converter.rb
@@ -1,7 +1,7 @@
 class RomanNumeralConverter
   def convert(number)
-    if number == 10
-      "X"
+    if number >= 10
+      "X" + ("I" * (number - 10))
     elsif number >= 5
       "V" + ("I" * (number - 5))
     else
