29b23cf438a54591fd0ea4ef3da6d04a7b20aceb
Tristan <tristan.gomez@gmail.com>
2013-06-28 16:38:55 +0800

    refactor

--- 20130628-roman-to-arabic/roman_to_arabic.rb
+++ 20130628-roman-to-arabic/roman_to_arabic.rb
@@ -1,11 +1,20 @@
 class RomanToArabic
+  VALUES_TABLE = [
+    ['X', 10],
+    ['V', 5],
+    ['I', 1]
+  ]
+
   def convert roman
     x_count = roman.count('X')
     v_count = roman.count('V')
     i_count = roman.count('I')
 
-    (x_count * 10) + 
-    (v_count * 5) + 
-    (i_count)
+    result = 0
+    VALUES_TABLE.each do |glyph, value|
+      result += roman.count(glyph) * value
+    end
+    result
   end
+
 end
